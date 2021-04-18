f6d274cc588d6f8ac1567c599ff75716b1a0c3d1
Maciek Pastuszak <maciek@macbook-pro-maciek.home>
2021-04-18 17:42:44 +0200

    add Campgroudn index

--- app.js
+++ app.js
@@ -26,11 +26,11 @@ app.get('/', (req, res) => {
     res.render('home')
 })
 
-app.get('/makecampground', async (req, res) => {
-    const camp = new Campground({ title: 'My backyard', description: 'Cheap camping' });
-    await camp.save();
-    res.send(camp)
+app.get('/campgrounds', async (req, res) => {
+    const campgrounds = await Campground.find({})
+    res.render('campgrounds/index', { campgrounds })
 })
+
 
 app.listen(3000, () => {
     console.log("SERVING ON PORT 3000")
