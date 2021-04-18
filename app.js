57a918d15ec40fba8d41878786c653ad8530b3ec
Maciek Pastuszak <maciek@macbook-pro-maciek.home>
2021-04-18 22:48:30 +0200

    add Campground New and Create

--- app.js
+++ app.js
@@ -22,6 +22,8 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+app.use(express.urlencoded({ extended: true }))
+
 app.get('/', (req, res) => {
     res.render('home')
 })
@@ -31,11 +33,20 @@ app.get('/campgrounds', async (req, res) => {
     res.render('campgrounds/index', { campgrounds })
 })
 
+app.get('campgrounds/new', (req, res) => {
+    res.render('campgrounds/new')
+});
+
+app.post('campgrounds', async (req, res) => {
+    const campground = new Camground(req.body.campground);
+    await campground.save();
+    res.redirect(`/campground/${campground._id}`)
+})
+
 app.get('/campgrounds/:id', async (req, res) => {
     const campground = await Campground.findById(req.params.id)
     res.render('campgrounds/show', { campground });
 })
-
 
 app.listen(3000, () => {
     console.log("SERVING ON PORT 3000")
