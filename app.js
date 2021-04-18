0e1f660b738a2c2f65a6a6c7a0f749c6439e89ac
Maciek Pastuszak <maciek@macbook-pro-maciek.home>
2021-04-18 18:16:11 +0200

    add Campgroudn Show

--- app.js
+++ app.js
@@ -31,6 +31,11 @@ app.get('/campgrounds', async (req, res) => {
     res.render('campgrounds/index', { campgrounds })
 })
 
+app.get('/campgrounds/:id', async (req, res) => {
+    const campground = await Campground.findById(req.params.id)
+    res.render('campgrounds/show', { campground });
+})
+
 
 app.listen(3000, () => {
     console.log("SERVING ON PORT 3000")
