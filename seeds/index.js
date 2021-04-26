4ccdeacc4f1a6c9a15e31a5c69fe4bcf850bee83
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2021-04-27 01:12:20 +0200

    style campground index

--- seeds/index.js
+++ seeds/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const cities = require('./cities');
-const { places, descriptors } = require('./seedHelpers')
+const { places, descriptors } = require('./seedHelpers');
 const Campground = require('../models/campground');
 
 
@@ -13,10 +13,10 @@ mongoose.connect('mongodb://localhost:27017/yelp-camp1', {
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", () => {
-    console.log("Database connected")
+    console.log("Database connected");
 });
 
-const sample = (array) => array[Math.floor(Math.random() * array.length)];
+const sample = array => array[Math.floor(Math.random() * array.length)];
 
 const seedDB = async () => {
     await Campground.deleteMany({});
@@ -35,5 +35,5 @@ const seedDB = async () => {
 }
 
 seedDB().then(() => {
-    mongoose.connection.close()
+    mongoose.connection.close();
 })
