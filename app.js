b29af0053ea02ba5a172dfa2ebaa6949122ef816
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2021-05-04 12:50:13 +0200

    handle more errors

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
-const catchAsync = require('./utils/catchAsync')
+const catchAsync = require('./utils/catchAsync');
+const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
 const Campground = require('./models/campground');
 
@@ -42,6 +43,7 @@ app.get('/campgrounds/new', (req, res) => {
 });
 
 app.post('/campgrounds', catchAsync(async (req, res, next) => {
+    if (!req.body.campground) throw new ExpressError('Invalid Campground Data', 400);
     const campground = new Campground(req.body.campground);
     await campground.save();
     res.redirect(`/campgrounds/${campground._id}`)
@@ -69,8 +71,13 @@ app.delete('/campgrounds/:id', catchAsync(async (req, res) => {
     res.redirect('/campgrounds');
 }));
 
+app.all('*', (req, res, next) => {
+    next(new ExpressError('Page Not Founds', 404))
+});
+
 app.use((err, req, res, next) => {
-    res.send('Oh boy, somethingg went wrong!')
+    const { statusCode = 500, message = "Something went wrong" } = err;
+    res.status(statusCode).send(message)
 });
 
 app.listen(3000, () => {
