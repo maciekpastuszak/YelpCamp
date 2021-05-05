94f5978250601d0c796814a8844ff9f1de540e5d
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2021-05-05 20:44:52 +0200

    create review route

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const catchAsync = require('./utils/catchAsync');
 const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
 const Campground = require('./models/campground');
+const Review = require('./models/review')
 const { validate } = require('./models/campground');
 
 
@@ -83,6 +84,15 @@ app.delete('/campgrounds/:id', catchAsync(async (req, res) => {
     await Campground.findByIdAndDelete(id);
     res.redirect('/campgrounds');
 }));
+
+app.post('/campgrounds/:id/reviews', catchAsync(async (req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    const review = new Review(req.body.review);
+    campground.reviews.push(review);
+    await review.save();
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`);
+}))
 
 app.all('*', (req, res, next) => {
     next(new ExpressError('Page Not Founds', 404))
