84f94340206b6b8c65e45eff092c92f0caf81c9e
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2021-05-11 10:05:31 +0200

    add isLoggedIn middleware

--- routes/users.js
+++ routes/users.js
@@ -10,14 +10,14 @@ router.get('/register', (req,res) => {
 
 router.post('/register', catchAsync(async (req,res) =>{
     try {
-    const {email, username, password} = req.body;
-    const user = new User({email, username});
-    const registeredUser = await User.register(user, password);
-    req.flash('success', 'Welcome to Yelp Camp!');
-    res.redirect('./campgrounds')
-} catch(e){
-    req.flash('error', e.message);
-    res.redirect('register')
+        const {email, username, password} = req.body;
+        const user = new User({email, username});
+        const registeredUser = await User.register(user, password);
+        req.flash('success', 'Welcome to Yelp Camp!');
+        res.redirect('./campgrounds')
+    } catch(e){
+        req.flash('error', e.message);
+        res.redirect('register')
 }
 
 }));
