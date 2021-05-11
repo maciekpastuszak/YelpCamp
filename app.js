ebe5bf29fe695c401677e837c18b8508570a3cb8
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2021-05-11 08:45:28 +0200

    create user model and configure passport

--- app.js
+++ app.js
@@ -6,9 +6,13 @@ const session = require('express-session');
 const flash = require('connect-flash');
 const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const User = require('./models/user');
 
 const campgrounds = require('./routes/campgrounds');
 const reviews = require('./routes/reviews');
+const { getMaxListeners } = require('./models/user');
 
 // const Campground = require('./models/campground');
 // const Review = require('./models/review');
@@ -52,10 +56,24 @@ const sessionConfig = {
 app.use(session(sessionConfig));
 app.use(flash());
 
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
 app.use((req, res, next) => {
     res.locals.success = req.flash('success');
     res.locals.error = req.flash('error');
     next();
+})
+
+app.get('/fakeuser', async (req,res) => {
+    const user = new User({email: 'mp@gmail.com', username: 'paciek'});
+    const newUser = await User.register(user, 'paciek1');
+    res.send(newUser);
 })
 
 app.use('/campgrounds', campgrounds)
