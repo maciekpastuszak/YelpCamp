46ed21796761a21267fe170a07e729ccbeacb7d9
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2021-05-11 09:50:18 +0200

    add login routes

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
 const catchAsync = require('../utils/catchAsync');
 const User = require('../models/user');
 
@@ -20,5 +21,15 @@ router.post('/register', catchAsync(async (req,res) =>{
 }
 
 }));
+
+router.get('/login', (req,res) => {
+res.render('users/login')
+
+});
+
+router.post('/login', passport.authenticate('local', {failureFlash: true, failureRedirect: '/login'}), (req,res) => {
+req.flash('success', 'Welcome back!');
+res.redirect('/campgrounds');
+})
 
 module.exports = router;
