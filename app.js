4b70970f0693e207575e3b4aa5192c0f738c29af
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2021-05-13 08:49:05 +0200

    show and hide edit/delete buttons and add campground permissions

--- app.js
+++ app.js
@@ -72,11 +72,6 @@ app.use((req, res, next) => {
     next();
 })
 
-// app.get('/fakeuser', async (req,res) => {
-//     const user = new User({email: 'mp@gmail.com', username: 'paciek'});
-//     const newUser = await User.register(user, 'paciek1');
-//     res.send(newUser);
-// })
 app.use('/', userRoutes)
 app.use('/campgrounds', campgroundRoutes)
 app.use('/campgrounds/:id/reviews', reviewRoutes)
--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -45,17 +45,27 @@ router.get('/:id', catchAsync(async (req, res) => {
 }));
 
 router.get('/:id/edit', isLoggedIn, catchAsync(async (req, res) => {
-    const campground = await Campground.findById(req.params.id)
+    const {id }= req.params;
+    const campground = await Campground.findById(id)
     if (!campground) {	
         req.flash('error', 'Cannot find that campground!');	
         return res.redirect('/campgrounds');	
+    }
+    if (!campground.author.equals(req.user._id)) {
+        req.flash('error', 'You do not have permission to do that')
+        res.redirect(`/campgrounds/${id}`)
     }
     res.render('campgrounds/edit', { campground });
 }));
 
 router.put('/:id', isLoggedIn, validateCampground, catchAsync(async (req, res) => {
     const { id } = req.params;
-    const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
+    const campground = await Campground.findById(id);
+    if (!campground.author.equals(req.user._id)) {
+        req.flash('error', 'You do not have permission to do that')
+        res.redirect(`/campgrounds/${id}`)
+    }
+    const camp = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
     req.flash('success', 'Succesfully updated campground')
     res.redirect(`/campgrounds/${campground._id}`)
 }));
