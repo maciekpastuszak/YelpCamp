b644b9daaa5f2fb91ce502f1d20cb6115a4b7223
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2021-05-16 18:04:29 +0200

    refactor routes to router.route

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -7,18 +7,18 @@ const Campground = require('../models/campground');
 const Review = require('../models/review')
 const {isLoggedIn, isAuthor, validateCampground} = require('../middleware');
 
-router.get('/', catchAsync(campgrounds.index));
+
+router.route('/')
+.get(catchAsync(campgrounds.index))
+.post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground));
 
 router.get('/new',isLoggedIn, campgrounds.renderNewForm);
 
-router.post('/', isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground));
-
-router.get('/:id', catchAsync(campgrounds.showCampground));
+router.route('/:id')
+    .get(catchAsync(campgrounds.showCampground))
+    .put(isLoggedIn, isAuthor, validateCampground, catchAsync(campgrounds.updateCampground))
+    .delete(isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground));
 
 router.get('/:id/edit',isLoggedIn, isAuthor, catchAsync(campgrounds.renderEditCampground));
-
-router.put('/:id', isLoggedIn, isAuthor, validateCampground, catchAsync(campgrounds.updateCampground));
-
-router.delete('/:id', isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground));
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -5,14 +5,15 @@ const catchAsync = require('../utils/catchAsync');
 const User = require('../models/user');
 const users = require('../controllers/users');
 
-router.get('/register', users.renderRegister);
 
-router.post('/register', catchAsync(users.register));
+router.route('/register')
+    .get(users.renderRegister)
+    .post(catchAsync(users.register));
 
-router.get('/login', users.renderLogin);
-
-router.post('/login', passport.authenticate('local', {failureFlash: true, failureRedirect: '/login'}), users.login)
-
+router.route('/login')
+    .get(users.renderLogin)
+    .post(passport.authenticate('local', {failureFlash: true, failureRedirect: '/login'}), users.login)
+	
 router.get('/logout', users.logout)
 
 module.exports = router;
