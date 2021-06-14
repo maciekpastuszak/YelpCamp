5eee38b28c581f5ba6bf6c3c2b0074021c1956f7
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2021-06-14 08:23:08 +0200

    use express-mongo-sanitize and sanite-html packages

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ const methodOverride = require('method-override');
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
 const User = require('./models/user');
+const mongoSanitize = require('express-mongo-sanitize');
 
 const userRoutes = require('./routes/users')
 const campgroundRoutes = require('./routes/campgrounds');
@@ -47,6 +48,9 @@ app.set('views', path.join(__dirname, 'views'))
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(mongoSanitize({
+    replaceWith: '_'
+}));
 
 const sessionConfig = {
     secret: 'thisshouldbeabettersecret!',
